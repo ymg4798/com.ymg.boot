724f37b43e4739c6cb76612a27bc7087f3e0518a
ymk <ymg4798@naver.com>
2022-09-28 20:32:26 +0900

    spring batch step예제 수행

--- src/main/java/com/ymg/boot/springboot/batch/sampleJop.java
+++ src/main/java/com/ymg/boot/springboot/batch/sampleJop.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,16 +22,30 @@ public class sampleJop {
     @Bean
     public Job sample() {
         return jobBuilderFactory.get("sample")// sample batch job 생성
-                .start(simpleStep1())
+                .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
     @Bean
-    public Step simpleStep1(){
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate){
         return stepBuilderFactory.get("simpleStep1")// simpleStep batch step설정
                 .tasklet((contribution, chunkContext) -> {// step에서 단일 수행 커스텀 기능
                     log.info(">>> step1");
+                    log.info(">>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                }).build();
     }
 }
