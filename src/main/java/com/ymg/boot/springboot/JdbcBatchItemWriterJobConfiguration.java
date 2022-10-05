4a49ab1b3c1502ff4ab62fb8841116b9fac9e21e
ymk <ymg4798@naver.com>
2022-10-05 20:29:08 +0900

    spring batch학습 2022-10-05

---
+++ src/main/java/com/ymg/boot/springboot/JdbcBatchItemWriterJobConfiguration.java
@@ -0,0 +1,74 @@
+package com.ymg.boot.springboot;
+
+import com.ymg.boot.springboot.domain.posts.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcBatchItemWriterJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    private static final int chunkSize=10;
+
+    @Bean
+    public Job jdbcBatchItemWriterJob(){
+
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep() {
+        return stepBuilderFactory.get("jdbcBatchItemWriterStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(jdbcBatchItemWriterReader())
+                .writer(jdbcBatchItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<Pay> jdbcBatchItemWriterReader() {
+        return new JdbcCursorItemReaderBuilder<Pay>()
+                .fetchSize(chunkSize)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .sql("SELECT id, amount, tx_name, tx_date_time FROM pay")
+                .name("jdbcBatchItemWriter")
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Pay> jdbcBatchItemWriter() {
+        /*
+        return new JdbcBatchItemWriterBuilder<Map<String, Object>>()
+                .columnMapped()
+                .dataSource(this.dataSource)
+                .sql("insert into pay2(amount, tx_name, tx_data_time) values (:amount, :txName, :txDataTime)")
+                .build();
+         */
+        /*
+        return new JdbcBatchItemWriterBuilder<Pay>()
+                .dataSource(dataSource)
+                .sql("insert into pay2(amount, tx_name, tx_date_time) values (:amount, :txName, :txDateTime)")
+                .beanMapped()
+                .build();
+        */
+    }
+
+}
